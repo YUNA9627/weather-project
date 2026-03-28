42634d7c3e26280e535b902132f353e189b83d2c
YUNA9627 <yuna2175@naver.com>
2026-03-28 21:47:07 +0900

    ui : 날씨 아이콘 추가

--- src/component/WeatherBox.jsx
+++ src/component/WeatherBox.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 export const WeatherBox = ({weather}) => {
+  const temp = weather?.main?.temp
   return (
     <>
         <div className="city">{weather && weather.name}</div>
+        <div>
+          <img src="https://www.emojiall.com/images/120/skype/1.2/1f324-fe0f.png" alt="" />
+        </div>
         {/* <div>{weather?.name}</div> */}
-        <h2 className="temp">{weather?.main.temp}° / {(weather?.main.temp)*1.8+32}화씨</h2>
+        <h2 className="temp">{temp}° / {(temp * 1.8 + 32).toFixed(1)}°F</h2>
         <h4>{weather?.weather[0].description}</h4>
     </>
   )
