29757ec07a8209919b0a6ad96e18faa65a4be009
YUNA9627 <yuna2175@naver.com>
2026-03-28 21:47:33 +0900

    ui : 클릭된 버튼 표시 추가

--- src/component/WeatherBtn.jsx
+++ src/component/WeatherBtn.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
-const WeatherBtn = ({ cities, setCity }) => {
+const WeatherBtn = ({ cities, setCity, currentLocation, selectCity }) => {
   console.log("cities?",cities)
   return (
     <div>
-      <button>Current Location</button>
+      <button
+        onClick={() => currentLocation("current")}
+        className={selectCity === '' ? 'weather-btn active' : 'weather-btn'}
+      >
+        Current Location
+      </button>
       {cities.map((item)=>(
-        <button onClick={()=>setCity(item)}>{item}</button>
+        <button
+          onClick={()=>setCity(item)}
+          className={selectCity === item ? 'weather-btn active' : 'weather-btn'}
+        >
+          {item}
+        </button>
       ))}
     </div>
   )
